b321d9fcd85eaeb54e0cc62f13f6ed7f44d877cd
Ciara Brown <cnbrown2021@gmail.com>
2021-10-19 15:04:05 -0400

    Identity Model

---
+++ 24Hour.Data/Users.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24Hour.Data
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public Guid AutherId { get; set; }
+
+    }
+}
